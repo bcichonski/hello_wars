d4772d26ae362d51998122771e60b8982327663f
bartq <bci@poczta.onet.pl>
2017-03-16 20:15:51 +0100

    slightly better approach imho

--- Arena/src/Arena/ViewModels/WinnerSelectionViewModel.cs
+++ Arena/src/Arena/ViewModels/WinnerSelectionViewModel.cs
@@ -7,12 +7,19 @@ using System.Windows.Input;
 using Arena.Commands;
 using Common.Interfaces;
 using Common.Utilities;
+using Common.Models;
 
 namespace Arena.ViewModels
 {
     public class WinnerSelectionViewModel : BindableBase
     {
         private ICompetitor _selectedWinner;
+        private static readonly IEnumerable<ICompetitor> defaultOption = new List<ICompetitor>() {
+            new Competitor() {
+                Name = "Nobody wins"
+            }
+        };
+
         private IEnumerable<ICompetitor> _competitors;
 
         public ICompetitor SelectedWinner
@@ -23,13 +30,14 @@ namespace Arena.ViewModels
         }
         public IEnumerable<ICompetitor> Competitors 
         {
-            get { return _competitors; }
+            get { return defaultOption.Concat(_competitors); }
             set { SetProperty(ref _competitors, value); }
         }
 
         public WinnerSelectionViewModel(IEnumerable<ICompetitor> competitors )
         {
             Competitors = competitors;
+            SelectedWinner = Competitors.First();
         }
     }
 }
